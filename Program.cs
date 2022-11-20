167f5dcd92e0182535c6b42da3f733ffb7b00f50
chenaoh <jfarova112333@gmail.com>
2022-11-19 21:41:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    
+    {
+        
+   int numeros; 
+  int total=0;
+  int i;
+  
+  Console.WriteLine("Introduzca 8 números: ");
+  for (i=0; i<8; i++){
+    numeros=int.Parse(Console.ReadLine());
+    total=total + numeros;
+  }
+
+  Console.WriteLine("La suma de los números es "+total);
+
+        }
+        
+    }
+
